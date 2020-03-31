eb3f2f6b65d4e7ce91126d417bbc5d55f7d10cdb
Andrés Sanz Buezo <sanzbuezo@hotmail.com>
2020-03-31 21:34:47 +0200

    Backend - DAO - User
    Rollback changes removed by Ilyes

--- application/back-end/api/v1/_dao/user-dao.php
+++ application/back-end/api/v1/_dao/user-dao.php
@@ -5,7 +5,7 @@ include 'base-dao.php';
 /**
  * This function will return all the movies from the database
  */
-function getAllUsersDAO(){
+function getAllUsers(){
     $table = 'user';
     $statement = '';
 
@@ -17,6 +17,39 @@ function getAllUsersDAO(){
     } 
 
     return $users;    
+}
+
+/**
+ * This function will save the new user in the database
+ * @param string $firstname the name of the user
+ * @param string $lastname the family name of the user
+ * @param string $email the valid and unique email for the user 
+ * @param string $password the encrypted password the user entered
+ */
+function addNewUser($firstname, $lastname, $email, $password){
+    $table = 'user';
+    $columns = array("firstname", "lastname", "email", "password");
+    $values = array($firstname, $lastname, $email, $password);
+
+    $results = doInsert($table, $columns, $values);
+
+    return $results;
+}
+
+/**
+ * This function will get only one user if the email matches (email is UNIQUE).
+ * @param string $email the valid and unique email for the user 
+ * @return user 
+ */
+function getUserByEmail($email){
+    $table = 'user';
+    $statement = 'WHERE email LIKE "' . $email . '"';
+
+    $results = doSelect($table, $statement);
+
+    $user = mysqli_fetch_assoc($results);
+
+    return $user;  
 }
 
 ?>
