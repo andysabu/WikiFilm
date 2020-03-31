70abcb2593eb7ab5184e844d3bbd10f040de5004
Andrés Sanz Buezo <sanzbuezo@hotmail.com>
2020-03-31 17:18:13 +0200

    SQL - script - Movies
    Add some movies to the DB

---
+++ application/back-end/DB scripts/insert-movies.sql
@@ -0,0 +1,27 @@
+INSERT INTO movie(name, synopsis, release_date, director, poster) VALUES (
+    'Jaws',
+    'When a killer shark unleashes chaos on a beach community, it\'s up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.',
+    '1975-12-26',
+    'Steven Spielberg',
+    'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTZxcYJsAkzWhG1vI7aqs7endQsNgzy8dGAPSDuHqGPePuCFtIf');
+INSERT INTO movie(name, synopsis, release_date, director, poster) VALUES (
+    'Taxi Driver',
+    'A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action by attempting to liberate a presidential campaign worker and an underage prostitute.',
+    '1976-08-19',
+    'Martin Scorsese',
+    'https://m.media-amazon.com/images/M/MV5BM2M1MmVhNDgtNmI0YS00ZDNmLTkyNjctNTJiYTQ2N2NmYzc2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,658,1000_AL_.jpg');
+INSERT INTO movie(name, synopsis, release_date, director, poster) VALUES (
+    'Scarface',
+    'In 1980 Miami, a determined Cuban immigrant takes over a drug cartel and succumbs to greed',
+    '1984-02-10',
+    'Brian De Palma',
+    'https://m.media-amazon.com/images/M/MV5BNjdjNGQ4NDEtNTEwYS00MTgxLTliYzQtYzE2ZDRiZjFhZmNlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,666,1000_AL_.jpg');
+INSERT INTO movie(name, synopsis, release_date, director, poster) VALUES (
+    'Killing Eve ',
+    'After a series of events, the lives of a security operative and an assassin become inextricably linked.',
+    '2018-06-01',
+    'Phoebe Waller-Bridge',
+    'https://m.media-amazon.com/images/M/MV5BZDJmODFjMzEtNTE4MS00OGViLTk4OGYtZjg3OGFhM2VlOTliXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_SX675_AL_.jpg');
+
+
+SELECT * FROM `sql7329944`.`movie` LIMIT 1000;
