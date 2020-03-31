ce53c90b7116e5297c0154d962bfcf0fa2a3a41f
Andrés Sanz Buezo <sanzbuezo@hotmail.com>
2020-03-31 21:33:23 +0200

    Frontend - Navbar
    - Add side for responsive browsers

--- application/front-end/wiki-film/src/app/components/nav-bar/nav-bar.component.ts
+++ application/front-end/wiki-film/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { User } from 'src/app/model/wiki-film';
+
+declare const M;
 
 @Component({
   selector: "film-nav-bar",
@@ -7,12 +10,31 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 })
 export class NavBarComponent implements OnInit {
   
+  user = <User> {}
+
   @Output()
   emitterSession = new EventEmitter<boolean>();
   
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUser();
+    this.initSideBar();
+  }
+
+  initSideBar() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  }
+
+  getUser() {
+    const stringUser = localStorage.getItem('wf-user');
+    this.user = JSON.parse(stringUser);
+  }
+
+  showAlert(){
+    M.toast({html: 'This functionallity is not available!'})
+  }
 
   /**
    * This function will inform the application to close the session
