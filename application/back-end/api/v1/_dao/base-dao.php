649d85051a3f5bd1041692f32ae2045a55ed4a5b
Andrés Sanz Buezo <sanzbuezo@hotmail.com>
2020-04-01 13:49:26 +0200

    Backend - DAO - Base
    Rollback changes removed by Ilyes

--- application/back-end/api/v1/_dao/base-dao.php
+++ application/back-end/api/v1/_dao/base-dao.php
@@ -2,6 +2,7 @@
 
 // Set the parameters to access the Database
 include '../../../../config/database-config.php';
+include 'utils-dao.php';
 
 /**
  * This function will get all the entries which match with the parsed statement for a specific table.
@@ -15,8 +16,17 @@ function doSelect($table, $statement)
     return runQuery($query);
 }
 
-function doInsert()
+/**
+ * This function will generate the INSERT query to be executed for the parsed table
+ * @param string $table nane of the table where the INSERT Will executed
+ * @param array|string $columns affected columns
+ * @param array|string $values data for that columns
+ */
+function doInsert($table, $columns, $values)
 {
+    $stringValues = buildValuesString($values);
+    $query = 'INSERT INTO ' . $table . ' (' . implode(", ", $columns) . ') VALUES (' . $stringValues . ')';
+    return runQuery($query);
 }
 
 function doUpdate()
@@ -32,7 +42,8 @@ function doDelete()
  * @param string $query An SQL query
  * @return mysqli_result|bool A mysqli_result object or TRUE. Returns FALSE on failure.
  */
-function runQuery($query) {
+function runQuery($query)
+{
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
     $result = false;
@@ -46,5 +57,3 @@ function runQuery($query) {
 
     return $result;
 }
-
-?>
